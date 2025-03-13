fd021f1a87839b39324f66367a592ac9807b217f
Azaiyah L <emrahhuremagic18@gmail.com>
2025-03-13 17:55:04 +0000

    Code update & .gitpod.yml commit

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -4,5 +4,52 @@
 
 
 // Start of the project - Math Game 
+// Math game - 4 different modes (add, substract, mulitply, divide), 5 rounds
 
 
+void Menu()
+{
+    Console.WriteLine("-----------------------------------------");
+    Console.WriteLine("1. Add");
+    Console.WriteLine("2. Substract");
+    Console.WriteLine("3. Mulitply");
+    Console.WriteLine("4. Divide");
+    Console.WriteLine("-----------------------------------------");
+}
+
+int Add()
+{
+    Random rand = new Random();
+    
+}
+
+
+Menu();
+
+Console.Write("\nThis is your choice: ");
+string choice = Console.ReadLine();
+
+
+switch (choice)
+{
+    case "1":
+        Console.WriteLine("Add");
+        break; 
+    
+    case "2":
+        Console.WriteLine("Add");
+        break; 
+    
+    case "3":
+        Console.WriteLine("Add");
+        break; 
+    
+    case "4":
+        Console.WriteLine("Add");
+        break; 
+    
+    default: 
+        Console.WriteLine("Fail");
+        break; 
+    
+}
--- MathGame/obj/Debug/net8.0/MathGame.AssemblyInfo.cs
+++ MathGame/obj/Debug/net8.0/MathGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2b48a220eb2ef562a9f40b86032ec24552dccf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e3690e39edbbd2fbdf0cd183d77c2487fb8f7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
